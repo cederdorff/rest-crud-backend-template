becbb385d5a8a49aa4107d3938134bdaf3bbbec6
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-10-31 14:00:25 +0100

    template projects

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+// Your github page origin has to be written EXACTLY like this! https://behu-kea.github.io
+const URL_FOR_FRONTEND = "YOUR_GITHUB_PAGE_ORIGIN_HERE";
+
+let users = require("./data"); // users array imported from data.js
+
+app.use(express.json()); //Used to parse JSON bodies
+app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
+
+// If the application is running localhost allow all requests,
+// otherwise add cors for specific website
+// Remember to add the NODE_ENV="prod" on server!
+const cors_url = process.env.NODE_ENV === "prod" ? URL_FOR_FRONTEND : "*";
+app.use(
+    cors({
+        origin: cors_url
+    })
+);
+
+// ========== REST API Implementation ========== //
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+// ========== READ: read all users from users ========== //
+// Todo
+
+// ========== READ: get user by id ========== //
+// Todo
+
+// ========== CREATE: create new user and add to users ========== //
+// Todo
+
+// ========== UPDATE: update existing user ========== //
+// Todo
+
+// ========== DELETE: delete user ========== //
+// Todo
+
+app.listen(port, () => {
+    console.log(`Node.js REST API listening at http://localhost:${port}`);
+});
